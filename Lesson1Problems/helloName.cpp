0bd4ea63104250a102255de1584a5eeb3cecdafe
liz <Lizandrocampos96@gmail.com>
2024-04-08 21:15:48 -0500

    Create first commit

---
+++ Lesson1Problems/helloName.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::string userInput = "";    
+    std::string fgkfg;
+    
+    std::cout << "please enter your name. ";
+    std::cin >> userInput;
+    
+     std::cout << "Hello, " << userInput << "!";
+
+       }
---
+++ Lesson1Problems/squareCreator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+    int userInput = 0;    
+    
+    std::cout << "please enter a number ";
+    std::cin >> userInput;
+     
+    for(int i = 0; i < userInput; i++) {
+      std::cout << std::endl;
+    for(int x = 0; x < userInput; x++){
+    std::cout << " * ";
+       }
+    }
+}
